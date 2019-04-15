1b8e38ee112b232dd29f85b57e40037375a228dc
zl777 <741966050@qq.com>
2019-04-15 09:10:38 +0800

    第二次

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,49 @@
+/*
+ * @Author: mikey.zhanglei 
+ * @Date: 2019-04-15 08:59:48 
+ * @Last Modified by: mikey.zhanglei
+ * @Last Modified time: 2019-04-15 09:08:44
+ */
 var gulp = require('gulp');
 var server = require('gulp-webserver');
-var miniCss = require('gulp-clone-css');
-var miniScript = require('gulp-ugilfy');
+var miniCss = require('gulp-clean-css');
+var miniScript = require('gulp-uglify');
 var miniImg = require('gulp-imagemin');
-var gulp = require('gulp');
-var gulp = require('gulp');
+
+//开启服务
+gulp.task('webserver', () => {
+    return gulp.src('./')
+        .pipe(server({
+            host: 'localhost',
+            port: 8080,
+            open: true
+
+        }))
+})
+
+//压缩css
+gulp.task('miniCss', () => {
+    return gulp.src('./src/css/*.css')
+        .pipe(miniCss())
+        .pipe(gulp.dest('dist/css'))
+})
+
+
+//压缩js
+gulp.task('miniScript', () => {
+    return gulp.src('./src/js/*.js')
+        .pipe(miniScript())
+        .pipe(gulp.dest('dist/js'))
+})
+
+//压缩图片
+gulp.task('miniImg', () => {
+    return gulp.src('./src/image/*')
+        .pipe(miniImg())
+        .pipe(gulp.dest('dist/image'))
+})
+
+//监听
+// gulp.task('watch',
+//     gulp.watch('./src/css/*.css', gulp.series('css'))
+// )
